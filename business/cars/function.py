0159111447cde3a6ef68b47c9280f028ab27997a
pastitayika <diosdelsexo@pafcat.net>
2024-05-29 21:33:37 -0400

    modificacion de cars

--- business/cars/function.py
+++ business/cars/function.py
@@ -2,4 +2,13 @@ from .database import CarsDatabase
 
 def get_cars():
     cars = CarsDatabase.get_cars()
+
+    for car in cars:
+
+        fixed_error = car.license_plate
+
+        fixed_error = fixed_error.replace("ERROR_FORBIDDEN", "PATENTE NO DISPONIBLE")
+
+        car.license_plate = fixed_error
+
     return cars
