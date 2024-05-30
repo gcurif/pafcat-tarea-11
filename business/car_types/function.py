286c25eecdda635fcb5469f444e18e7b779c3301
pastitayika <diosdelsexo@pafcat.net>
2024-05-29 21:26:48 -0400

    modificacion de car_types

--- business/car_types/function.py
+++ business/car_types/function.py
@@ -3,4 +3,16 @@ from .database import CarTypesDatabase
 def get_car_types():
     car_types = CarTypesDatabase.get_car_types()
 
+    for car_type in car_types:
+
+            fixed_name_type = car_type.name
+
+            fixed_name_type =  fixed_name_type.strip()
+
+            fixed_name_type = fixed_name_type.capitalize()
+
+            fixed_name_type = fixed_name_type.replace('ï¿½', 'ñ')
+
+            car_type.name = fixed_name_type
+
     return car_types
