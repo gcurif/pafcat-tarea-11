33f03a02a5692724ef7d71581e62aa55ed71e00b
pastitayika <diosdelsexo@pafcat.net>
2024-05-29 21:37:31 -0400

    modificacion de insureds

--- business/insureds/function.py
+++ business/insureds/function.py
@@ -2,5 +2,18 @@ from .database import InsuredDatabase
 
 def get_insureds():
     insureds = InsuredDatabase.get_insureds()
+    for insured in insureds:
+        
+        fixed_name_insured = insured.name  
+
+        fixed_name_insured = fixed_name_insured.capitalize()
+
+        fixed_name_insured = fixed_name_insured.replace('ï¿½', 'ñ')
+
+        fixed_name_insured = fixed_name_insured.strip()
+
+        insured.name = fixed_name_insured
+
     return insureds
 
+    
